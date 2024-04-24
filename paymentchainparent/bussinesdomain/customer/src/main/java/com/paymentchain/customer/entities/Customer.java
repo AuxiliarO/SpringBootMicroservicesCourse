1451059a69501bf88ff2abe2b3f8d02aeb272e95
Billy <hola342354534@gmail.com>
2024-04-24 12:12:50 -0500

    Microservices course

---
+++ paymentchainparent/bussinesdomain/customer/src/main/java/com/paymentchain/customer/entities/Customer.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.paymentchain.customer.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ *
+ * @author Admin
+ */
+@Data
+@Entity
+public class Customer {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    @Id
+    private long id;
+    private String code;
+    private String name;
+    private String phone;
+    private String iban;
+    private String surname;
+    private String address;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CustomerProduct> products;
+    @Transient
+    private List<?> transactions;
+
+}
